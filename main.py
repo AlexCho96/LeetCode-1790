24f04d5e1d07ea9b29b5ea5e584f21299a5f8c6d
Alexandre Cho <alecho97@gmail.com>
2025-02-04 22:28:20 -0300

    Adjust main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from solution import Solution
 
 def main():
     sol = Solution()
-    print(sol.maxAscendingSum([1,4,3,3,2]))  # 2
+    print(sol.areAlmostEqual("bank", "kanb"))  # 2
 
 if __name__ == "__main__":
     main()
